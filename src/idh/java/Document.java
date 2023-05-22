a862f4a901f3243332fc214c99352a4bf799f991
hellykoster <hellykoster@macbook-pro.fritz.box>
2023-05-22 20:44:33 +0200

    i really tried :'(

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -3,11 +3,38 @@ package idh.java;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Document implements Iterable<String> {
+	
+	class DocumentIterator implements Iterator<String> {
+		Document docText;
+		int currentPosition = -1;
+		StringTokenizer st;
+		
+		public DocumentIterator(Document document) {
+			this.docText = document;
+			this.st = new StringTokenizer(docText.documentText);
+		}
+	
+		public boolean hasNext() {
+			return currentPosition < st.countTokens();
+		}
+	
+		public String next() {
+			currentPosition++;
+			tokenCounter++;
+			return (String) st.nextElement();
+		}
+		
+		
+	}
+	
 	String documentText;
+	int tokenCounter = 0;
 
 	public static Document readFromFile(File f) throws IOException {
 		FileReader fileReader = new FileReader(f);
@@ -31,34 +58,52 @@ public class Document implements Iterable<String> {
 		this.documentText = documentText;
 	}
 	
+	public int getTokenCounter() {
+		return tokenCounter;
+	}
+	
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("data/dracula.txt"));
-		int i = 0;
-		for (String token : d) {
-			System.out.println(i++ + ": " + token + " ");
-			if (i > 100)
-				break;
-		}
-	}
-
-	@Override
-	public Iterator<String> iterator() {
-		return new Iterator<String>() {
-
-			StringTokenizer tokenizer = new StringTokenizer(documentText);
-			
-			@Override
-			public boolean hasNext() {
-				return tokenizer.hasMoreTokens();
-			}
-
-			@Override
-			public String next() {
-				return tokenizer.nextToken();
-			}
-			
-		};
+		
+		/*
+		 * for (String docText : d) { System.out.println(docText);
+		 * 
+		 * }
+		 */
+		
+	
+			System.out.println(d.ttr());
+		
+	
 	}
 	
+
+
+	public Iterator<String> iterator() {
+		return new DocumentIterator(this);
+	}
+	
+	public boolean equals(Object o) {
+		if (! (o instanceof String))
+			return false;
+		String s = (String) o;
+		String t = String.valueOf(this);
+		return s.equalsIgnoreCase(t);
+	}
+	
+	
+ 	
+	
+	public double ttr() {
+		Set<String> trrSet = new HashSet<String>();
+		trrSet.add(this.iterator().next());
+		int setSize = trrSet.size();
+		
+		int tokenCounter = this.getTokenCounter();
+		
+		double trr = setSize/tokenCounter;
+		return trr;
+	}
 	
 }
+
--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -2,16 +2,63 @@ package idh.java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Hanoi {
-
+	Queue<Integer> zahlenStart;
+	Queue<Integer> mitte;
+	Queue<Integer> rechts;
+	int[] z = {9,8,7,6,5,4,3};
+	int[] m = new int[z.length];
+	int[] r = new int[z.length];
+	
+	
+	
 	public Hanoi() {
-		// TODO: Implement
+		this.zahlenStart = new LinkedList<Integer>();
+		this.mitte = new LinkedList<Integer>();
+		this.rechts = new LinkedList<Integer>();
+		
+		zahlenStart.add(z[0]);
+		zahlenStart.add(z[1]);
+		zahlenStart.add(z[2]);
+		zahlenStart.add(z[3]);
+		zahlenStart.add(z[4]);
+		zahlenStart.add(z[5]);
+		zahlenStart.add(z[6]);
+
+		this.toString();
 	}
 	
 	private void movePiece(char from, char to) {
-		// TODO: Implement
+		String s = new StringBuilder().append(from).append(to).toString();
+		switch(s) {
+			case "lm":
+				mitte.add(zahlenStart.remove());
+				break;
+			case "lr":
+				rechts.add(zahlenStart.remove());
+				break;
+			case "ml":
+				zahlenStart.add(mitte.remove());
+				break;
+			case "mr":
+				rechts.add(mitte.remove());
+				break;
+			case "rl":
+				zahlenStart.add(rechts.remove());
+				break;
+			case "rm":
+				mitte.add(rechts.remove());
+				break;	
+			default:
+				System.out.println("Something must went wrong here");
+				
+		}
+		
 	}
 	
 	public void run() {
@@ -34,18 +81,56 @@ public class Hanoi {
 	}
 	
 	private Iterator<Integer> getLeftDescendingIterator() {
-		// TODO: Implement
-		return null;
+		return new Iterator<Integer>() {
+		public boolean hasNext() {
+			return zahlenStart.peek() != null;
+		}
+
+		public Integer next() {
+			StringBuilder t = new StringBuilder();
+			for (int i : z)
+		    {
+		         t.append(i);
+		    }
+			return Integer.parseInt(t.toString());
+			
+			
+		}
+		};
 
 	}
 	private Iterator<Integer> getMiddleDescendingIterator() {
-		// TODO: Implement
-		return null;
+		return new Iterator<Integer>() {
+			public boolean hasNext() {
+				return mitte.peek() != null;
+			}
+
+			public Integer next() {
+				StringBuilder t = new StringBuilder();
+				for (int i : m)
+			    {
+			         t.append(i);
+			    }
+				return Integer.parseInt(t.toString());
+			}
+			};
 
 	}
 	private Iterator<Integer> getRightDescendingIterator() {
-		// TODO: Implement
-		return null;
+		return new Iterator<Integer>() {
+			public boolean hasNext() {
+				return rechts.peek() != null;
+			}
+
+			public Integer next() {
+				StringBuilder t = new StringBuilder();
+				for (int i : r)
+			    {
+			         t.append(i);
+			    }
+				return Integer.parseInt(t.toString());
+			}
+			};
 	}
 	
 	public String toString() {
