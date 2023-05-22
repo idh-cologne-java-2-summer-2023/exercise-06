24f29ab722fe321b2a6115e441d58c09a797a947
fauli <fauli@hubert.de>
2023-05-22 19:22:31 +0200

    Aufgabe 1

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -3,17 +3,49 @@ package idh.java;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.ArrayDeque;
 
 public class Hanoi {
+	ArrayDeque<Integer> l = new ArrayDeque<Integer>();
+	ArrayDeque<Integer> m = new ArrayDeque<Integer>();
+	ArrayDeque<Integer> r = new ArrayDeque<Integer>();
 
 	public Hanoi() {
-		// TODO: Implement
+		for (int i = 9; i > 0; i--) {
+			l.add(i);
+		}
 	}
-	
+
+	private ArrayDeque<Integer> getTower(char c) {
+		switch (c) {
+		case 'l':
+			return this.l;
+
+		case 'm':
+			return this.m;
+
+		case 'r':
+			return this.r;
+
+		default:
+			return null;
+
+		}
+	}
+
 	private void movePiece(char from, char to) {
-		// TODO: Implement
+		ArrayDeque<Integer> fromTower = getTower(from);
+		ArrayDeque<Integer> toTower = getTower(to);
+
+		if (fromTower.isEmpty()) {
+			System.out.println("The chosen source stick is empty!");
+		} else if (!toTower.isEmpty() && fromTower.peekFirst() > toTower.peekFirst()) {
+			System.out.println("The chosen piece is bigger than the top piece of the target stick!");
+		} else {
+			toTower.add(fromTower.pollLast());
+		}
 	}
-	
+
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
@@ -29,50 +61,49 @@ public class Hanoi {
 			} catch (Exception e) {
 				System.out.println("Try again, something's not right.");
 				// e.printStackTrace();
-			} 
+			}
 		}
 	}
-	
+
 	private Iterator<Integer> getLeftDescendingIterator() {
-		// TODO: Implement
-		return null;
+		return l.iterator();
 
 	}
+
 	private Iterator<Integer> getMiddleDescendingIterator() {
-		// TODO: Implement
-		return null;
+		return m.iterator();
 
 	}
+
 	private Iterator<Integer> getRightDescendingIterator() {
-		// TODO: Implement
-		return null;
+		return r.iterator();
 	}
-	
+
 	public String toString() {
 		StringBuilder b = new StringBuilder();
 		b.append("  |\n l|");
 		Iterator<Integer> iter;
 		iter = this.getLeftDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |\n m|");
 		iter = this.getMiddleDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |\n r|");
 		iter = this.getRightDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |");
 		return b.toString();
 	}
-	
+
 	public static void main(String[] args) {
 		Hanoi hanoi = new Hanoi();
 		hanoi.run();
