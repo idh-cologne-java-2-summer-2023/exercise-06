ef71691485b1fbddcd5cabb171096081f10478b7
Nick-Uzk <nickleo.roeder@googlemail.com>
2023-05-23 22:38:30 +0200

    Nick-Uzk

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -3,79 +3,118 @@ package idh.java;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Hanoi {
+    private Queue<Integer> leftStack;     // Stab l
+    private Queue<Integer> middleStack;   // Stab m
+    private Queue<Integer> rightStack;    // Stab r
 
-	public Hanoi() {
-		// TODO: Implement
-	}
-	
-	private void movePiece(char from, char to) {
-		// TODO: Implement
-	}
-	
-	public void run() {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
-			try {
-				System.out.println(this);
-				System.out.print("Enter source and target stick (will move top piece):");
-				String s = br.readLine();
-				if (s.matches("^([lmr])([lmr])$")) {
-					char source = s.charAt(0);
-					char target = s.charAt(1);
-					movePiece(source, target);
-				}
-			} catch (Exception e) {
-				System.out.println("Try again, something's not right.");
-				// e.printStackTrace();
-			} 
-		}
-	}
-	
-	private Iterator<Integer> getLeftDescendingIterator() {
-		// TODO: Implement
-		return null;
+    public Hanoi() {
+        leftStack = new LinkedList<>();
+        middleStack = new LinkedList<>();
+        rightStack = new LinkedList<>();
 
-	}
-	private Iterator<Integer> getMiddleDescendingIterator() {
-		// TODO: Implement
-		return null;
+        // Scheiben der Größen 9 bis 1 auf dem linken Stab platzieren
+        for (int i = 9; i >= 1; i--) {
+            leftStack.offer(i);
+        }
+    }
 
-	}
-	private Iterator<Integer> getRightDescendingIterator() {
-		// TODO: Implement
-		return null;
-	}
-	
-	public String toString() {
-		StringBuilder b = new StringBuilder();
-		b.append("  |\n l|");
-		Iterator<Integer> iter;
-		iter = this.getLeftDescendingIterator();
-		while(iter.hasNext()) {
-			b.append(iter.next());
-			b.append(' ');
-		}
-		b.append("\n  |\n m|");
-		iter = this.getMiddleDescendingIterator();
-		while(iter.hasNext()) {
-			b.append(iter.next());
-			b.append(' ');
-		}
-		b.append("\n  |\n r|");
-		iter = this.getRightDescendingIterator();
-		while(iter.hasNext()) {
-			b.append(iter.next());
-			b.append(' ');
-		}
-		b.append("\n  |");
-		return b.toString();
-	}
-	
-	public static void main(String[] args) {
-		Hanoi hanoi = new Hanoi();
-		hanoi.run();
-	}
+    private void movePiece(char from, char to) {
+        Queue<Integer> fromStack = getStackForChar(from);
+        Queue<Integer> toStack = getStackForChar(to);
 
+        if (fromStack == null || toStack == null) {
+            System.out.println("Ungültige Eingabe. Bitte Quell- und Zielstab angeben.");
+            return;
+        }
+
+        if (fromStack.isEmpty()) {
+            System.out.println("Der Quellstab ist leer. Es gibt keine Scheibe zum Bewegen.");
+            return;
+        }
+
+        if (!toStack.isEmpty() && fromStack.peek() > toStack.peek()) {
+            System.out.println("Ungültiger Zug. Eine größere Scheibe kann nicht auf eine kleinere Scheibe gelegt werden.");
+            return;
+        }
+
+        int disk = fromStack.poll();
+        toStack.offer(disk);
+    }
+
+    private Queue<Integer> getStackForChar(char c) {
+        if (c == 'l') {
+            return leftStack;
+        } else if (c == 'm') {
+            return middleStack;
+        } else if (c == 'r') {
+            return rightStack;
+        } else {
+            return null;
+        }
+    }
+
+    private Iterator<Integer> getDescendingIterator(Queue<Integer> stack) {
+        Stack<Integer> tempStack = new Stack<>();
+        Iterator<Integer> iterator = stack.iterator();
+
+        while (iterator.hasNext()) {
+            tempStack.push(iterator.next());
+        }
+
+        return tempStack.iterator();
+    }
+
+    public void run() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try {
+                System.out.println(this);
+                System.out.print("Geben Sie Quell- und Zielstab an (obere Scheibe wird verschoben): ");
+                String input = br.readLine();
+                if (input.matches("^([lmr])([lmr])$")) {
+                    char source = input.charAt(0);
+                    char target = input.charAt(1);
+                    movePiece(source, target);
+                } else {
+                    System.out.println("Ungültige Eingabe. Bitte Quell- und Zielstab angeben (z.B. lm).");
+                }
+            } catch (Exception e) {
+                System.out.println("Ein Fehler ist aufgetreten. Bitte versuchen Sie es erneut.");
+            }
+        }
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("  |\n l|");
+        Iterator<Integer> iter = getDescendingIterator(leftStack);
+        while (iter.hasNext()) {
+            sb.append(iter.next());
+            sb.append(' ');
+        }
+        sb.append("\n  |\n m|");
+        iter = getDescendingIterator(middleStack);
+        while (iter.hasNext()) {
+            sb.append(iter.next());
+            sb.append(' ');
+        }
+        sb.append("\n  |\n r|");
+        iter = getDescendingIterator(rightStack);
+        while (iter.hasNext()) {
+            sb.append(iter.next());
+            sb.append(' ');
+        }
+        sb.append("\n  |");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Hanoi hanoi = new Hanoi();
+        hanoi.run();
+    }
 }
