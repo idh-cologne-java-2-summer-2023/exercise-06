d2e7b865d143c828d48cbc68e8895fa43690557c
F-Rux <frux@smail.uni-koeln.de>
2023-05-23 03:37:42 +0200

    Aufgabe 1 done (hopefully)

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -2,18 +2,53 @@ package idh.java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Iterator;
 
 public class Hanoi {
 
+	ArrayDeque<Integer> l = new ArrayDeque<Integer>();
+	ArrayDeque<Integer> m = new ArrayDeque<Integer>();
+	ArrayDeque<Integer> r = new ArrayDeque<Integer>();
+
 	public Hanoi() {
-		// TODO: Implement
+		for (int i = 9; i >= 1; i--) {
+			this.l.push(i);
+		}
 	}
-	
+
+	private ArrayDeque<Integer> Tower(char from) {
+
+		switch (from) {
+		case 'l':
+			return this.l;
+
+		case 'm':
+			return this.m;
+
+		case 'r':
+			return this.r;
+
+		default:
+			return null;
+
+		}
+	}
+
 	private void movePiece(char from, char to) {
-		// TODO: Implement
+
+		ArrayDeque<Integer> fromT = this.Tower(from);
+		ArrayDeque<Integer> toT = this.Tower(to);
+
+		if (fromT.isEmpty()) {
+			System.out.println("This stick is empty");
+		} else if (!toT.isEmpty() && fromT.peekFirst() > toT.peekFirst()) {
+			System.out.println("Disk is too big to be put there");
+		} else {
+			toT.add(fromT.pollLast());
+		}
 	}
-	
+
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
@@ -24,55 +59,55 @@ public class Hanoi {
 				if (s.matches("^([lmr])([lmr])$")) {
 					char source = s.charAt(0);
 					char target = s.charAt(1);
-					movePiece(source, target);
+					this.movePiece(source, target);
 				}
 			} catch (Exception e) {
 				System.out.println("Try again, something's not right.");
 				// e.printStackTrace();
-			} 
+			}
 		}
 	}
-	
+
 	private Iterator<Integer> getLeftDescendingIterator() {
-		// TODO: Implement
-		return null;
+		return this.l.descendingIterator();
 
 	}
+
 	private Iterator<Integer> getMiddleDescendingIterator() {
-		// TODO: Implement
-		return null;
+		return this.m.descendingIterator();
 
 	}
+
 	private Iterator<Integer> getRightDescendingIterator() {
-		// TODO: Implement
-		return null;
+		return this.r.descendingIterator();
 	}
-	
+
+	@Override
 	public String toString() {
 		StringBuilder b = new StringBuilder();
 		b.append("  |\n l|");
 		Iterator<Integer> iter;
 		iter = this.getLeftDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |\n m|");
 		iter = this.getMiddleDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |\n r|");
 		iter = this.getRightDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |");
 		return b.toString();
 	}
-	
+
 	public static void main(String[] args) {
 		Hanoi hanoi = new Hanoi();
 		hanoi.run();
