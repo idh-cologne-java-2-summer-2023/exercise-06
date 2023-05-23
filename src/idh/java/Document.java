8b1f0f56ffe1a7204bf5e942da8d43ac55181ab0
l00gen <99175858+l00gen@users.noreply.github.com>
2023-05-23 13:18:34 +0200

    aufgabe2

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -3,7 +3,9 @@ package idh.java;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Document implements Iterable<String> {
@@ -31,6 +33,18 @@ public class Document implements Iterable<String> {
 		this.documentText = documentText;
 	}
 	
+	public double ttr() {
+		Set<String> types = new HashSet<>();
+		int tokenCount = 0;
+
+		for (String token : this) {
+			tokenCount++;
+			types.add(token);
+		}
+
+		return (double) types.size() / tokenCount;
+	}
+
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("data/dracula.txt"));
 		int i = 0;
@@ -39,26 +53,31 @@ public class Document implements Iterable<String> {
 			if (i > 100)
 				break;
 		}
+
+		double ttrValue = d.ttr();
+		System.out.println("TTR: " + ttrValue);
 	}
 
 	@Override
 	public Iterator<String> iterator() {
-		return new Iterator<String>() {
-
-			StringTokenizer tokenizer = new StringTokenizer(documentText);
-			
-			@Override
-			public boolean hasNext() {
-				return tokenizer.hasMoreTokens();
-			}
-
-			@Override
-			public String next() {
-				return tokenizer.nextToken();
-			}
-			
-		};
+		return new StringTokenizerIterator(new StringTokenizer(documentText));
 	}
 	
-	
+	private static class StringTokenizerIterator implements Iterator<String> {
+		private StringTokenizer tokenizer;
+
+		public StringTokenizerIterator(StringTokenizer tokenizer) {
+			this.tokenizer = tokenizer;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return tokenizer.hasMoreTokens();
+		}
+
+		@Override
+		public String next() {
+			return tokenizer.nextToken();
+		}
+	}
 }
