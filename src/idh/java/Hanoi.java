0cfe89fe4963de90ed72d0729e60914cbca6d678
hellykoster <hellykoster@macbook-pro.fritz.box>
2023-05-23 17:00:10 +0200

    Finished Hanoi, not crying anymore

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -3,32 +3,28 @@ package idh.java;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Collection;
+import java.util.Deque;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Hanoi {
-	Queue<Integer> zahlenStart;
-	Queue<Integer> mitte;
-	Queue<Integer> rechts;
-	int[] z = {9,8,7,6,5,4,3};
-	int[] m = new int[z.length];
-	int[] r = new int[z.length];
-	
-	
+	Deque<Integer> links;
+	Deque<Integer> mitte;
+	Deque<Integer> rechts;
 	
 	public Hanoi() {
-		this.zahlenStart = new LinkedList<Integer>();
+		this.links = new LinkedList<Integer>();
 		this.mitte = new LinkedList<Integer>();
 		this.rechts = new LinkedList<Integer>();
 		
-		zahlenStart.add(z[0]);
-		zahlenStart.add(z[1]);
-		zahlenStart.add(z[2]);
-		zahlenStart.add(z[3]);
-		zahlenStart.add(z[4]);
-		zahlenStart.add(z[5]);
-		zahlenStart.add(z[6]);
+		links.add(9);
+		links.add(8);
+		links.add(7);
+		links.add(6);
+		links.add(5);
+		links.add(4);
+		links.add(3);
 
 		this.toString();
 	}
@@ -37,22 +33,22 @@ public class Hanoi {
 		String s = new StringBuilder().append(from).append(to).toString();
 		switch(s) {
 			case "lm":
-				mitte.add(zahlenStart.remove());
+				mitte.add(links.removeLast());
 				break;
 			case "lr":
-				rechts.add(zahlenStart.remove());
+				rechts.add(links.removeLast());
 				break;
 			case "ml":
-				zahlenStart.add(mitte.remove());
+				links.add(mitte.removeLast());
 				break;
 			case "mr":
-				rechts.add(mitte.remove());
+				rechts.add(mitte.removeLast());
 				break;
 			case "rl":
-				zahlenStart.add(rechts.remove());
+				links.add(rechts.removeLast());
 				break;
 			case "rm":
-				mitte.add(rechts.remove());
+				mitte.add(rechts.removeLast());
 				break;	
 			default:
 				System.out.println("Something must went wrong here");
@@ -81,57 +77,17 @@ public class Hanoi {
 	}
 	
 	private Iterator<Integer> getLeftDescendingIterator() {
-		return new Iterator<Integer>() {
-		public boolean hasNext() {
-			return zahlenStart.peek() != null;
-		}
-
-		public Integer next() {
-			StringBuilder t = new StringBuilder();
-			for (int i : z)
-		    {
-		         t.append(i);
-		    }
-			return Integer.parseInt(t.toString());
-			
-			
-		}
-		};
-
+		return links.iterator();
 	}
+
 	private Iterator<Integer> getMiddleDescendingIterator() {
-		return new Iterator<Integer>() {
-			public boolean hasNext() {
-				return mitte.peek() != null;
+				return mitte.iterator();
 			}
-
-			public Integer next() {
-				StringBuilder t = new StringBuilder();
-				for (int i : m)
-			    {
-			         t.append(i);
-			    }
-				return Integer.parseInt(t.toString());
-			}
-			};
-
-	}
+			
 	private Iterator<Integer> getRightDescendingIterator() {
-		return new Iterator<Integer>() {
-			public boolean hasNext() {
-				return rechts.peek() != null;
-			}
-
-			public Integer next() {
-				StringBuilder t = new StringBuilder();
-				for (int i : r)
-			    {
-			         t.append(i);
-			    }
-				return Integer.parseInt(t.toString());
-			}
-			};
+		return rechts.iterator();
 	}
+
 	
 	public String toString() {
 		StringBuilder b = new StringBuilder();
