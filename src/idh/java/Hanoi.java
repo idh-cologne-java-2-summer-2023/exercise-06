bb619f337ae378dab625dc459aa18bd1b013172e
EricXKl <eric.c.kloeckner@gmail.com>
2023-05-23 20:42:49 +0200

    Exercise 6 done

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -2,18 +2,91 @@ package idh.java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Deque;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Hanoi {
+	private Deque<Integer> l = new LinkedList<Integer>();
+	private Deque<Integer> m = new LinkedList<Integer>();
+	private Deque<Integer> r = new LinkedList<Integer>();
 
 	public Hanoi() {
-		// TODO: Implement
+		for (int i = 9; i > 0; i--) {
+			l.add(i);
+		}
 	}
-	
+
 	private void movePiece(char from, char to) {
+		Integer lastNum;
 		// TODO: Implement
-	}
-	
+		// von l einen weg zu m drauf: Randfall
+		if (from == 'l') {
+
+			lastNum = l.peekLast();
+
+			if (to == 'm') {
+
+				if (m.isEmpty() || lastNum < m.peekLast()) {
+					l.removeLast();
+					m.add(lastNum);
+
+				}
+
+				else
+					System.out.print("(-) the stone is too big :(");
+			}
+
+			if (to == 'r') {
+
+				if (r.isEmpty() || lastNum < r.peekLast()) {
+					l.removeLast();
+					r.add(lastNum);
+				} else
+					System.out.print("(-) the stone is too big :(");
+				
+			}
+
+			} else if (from == 'm') {
+				if (to == 'l') {
+					lastNum = m.peekLast();
+
+					if (l.isEmpty() || lastNum < l.peekLast()) {
+						m.removeLast();
+						l.add(lastNum);
+					}
+				}
+				if (to == 'r') {
+					lastNum = m.peekLast();
+
+					if (r.isEmpty() || lastNum < r.peekLast()) {
+						m.removeLast();
+						r.add(lastNum);
+					}
+
+				}
+			} else if (from == 'r') {
+				if (to == 'm') {
+					lastNum = r.peekLast();
+
+					if (m.isEmpty() || lastNum < m.peekLast()) {
+						r.removeLast();
+						m.add(lastNum);
+
+					}
+				}
+				if (to == 'l') {
+					lastNum = r.removeLast();
+
+					if (l.isEmpty() || lastNum < l.peekLast()) {
+						r.removeLast();
+						l.add(lastNum);
+					}
+				}
+			}
+		}
+
+
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
@@ -27,53 +100,57 @@ public class Hanoi {
 					movePiece(source, target);
 				}
 			} catch (Exception e) {
-				System.out.println("Try again, something's not right.");
-				// e.printStackTrace();
-			} 
+
+				e.printStackTrace();
+			}
 		}
 	}
-	
+
 	private Iterator<Integer> getLeftDescendingIterator() {
+
 		// TODO: Implement
-		return null;
+		return l.iterator();
 
 	}
+
 	private Iterator<Integer> getMiddleDescendingIterator() {
 		// TODO: Implement
-		return null;
+		return m.iterator();
 
 	}
+
 	private Iterator<Integer> getRightDescendingIterator() {
 		// TODO: Implement
-		return null;
+		return r.iterator();
 	}
-	
+
 	public String toString() {
 		StringBuilder b = new StringBuilder();
 		b.append("  |\n l|");
 		Iterator<Integer> iter;
 		iter = this.getLeftDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |\n m|");
 		iter = this.getMiddleDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |\n r|");
 		iter = this.getRightDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |");
 		return b.toString();
 	}
-	
+
 	public static void main(String[] args) {
+
 		Hanoi hanoi = new Hanoi();
 		hanoi.run();
 	}
