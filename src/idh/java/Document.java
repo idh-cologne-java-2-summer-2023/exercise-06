09eec075e1c23f6c34ab5656a62a2d27f9e32979
tinatomic <tinatomic@mbp-von-tina.fritz.box>
2023-05-23 20:35:28 +0200

    Session 6

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -1,13 +1,17 @@
+
 package idh.java;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Document implements Iterable<String> {
 	String documentText;
+	static int typeCounter = 0;
+	static int tokenCounter = 0;
 
 	public static Document readFromFile(File f) throws IOException {
 		FileReader fileReader = new FileReader(f);
@@ -33,16 +37,27 @@ public class Document implements Iterable<String> {
 	
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("data/dracula.txt"));
+		HashSet<String> bruh = new HashSet<String>();
+		
 		int i = 0;
 		for (String token : d) {
+			
+			if(bruh.add(token)){
+				bruh.add(token);
+				typeCounter++;
+			}
+				
 			System.out.println(i++ + ": " + token + " ");
-			if (i > 100)
-				break;
+			tokenCounter++;
+		//	if (i > 100)
+		//		break;
 		}
+		System.out.println(ttr());
 	}
 
 	@Override
 	public Iterator<String> iterator() {
+		//HashSet<String> bruh = new HashSet<String>();
 		return new Iterator<String>() {
 
 			StringTokenizer tokenizer = new StringTokenizer(documentText);
@@ -60,5 +75,8 @@ public class Document implements Iterable<String> {
 		};
 	}
 	
+	public static double ttr() {
+		return (double) typeCounter / (double) tokenCounter;
+	}
 	
 }
