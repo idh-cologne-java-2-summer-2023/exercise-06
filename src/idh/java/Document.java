6c37be81269bbc6928bfd982892b5a1fb4eadcbc
Me9Rs <MR1303@web.de>
2023-05-23 14:40:31 +0200

    ADDED:	Exercises 1 & 2

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -3,7 +3,9 @@ package idh.java;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Document implements Iterable<String> {
@@ -13,16 +15,16 @@ public class Document implements Iterable<String> {
 		FileReader fileReader = new FileReader(f);
 		int ch;
 		StringBuilder b = new StringBuilder();
-		while( (ch = fileReader.read()) != -1 ) {
+		while ((ch = fileReader.read()) != -1) {
 			b.append((char) ch);
 		}
 		fileReader.close();
 		Document doc = new Document();
 		doc.documentText = b.toString();
-		
+
 		return doc;
 	}
-	
+
 	public String getDocumentText() {
 		return documentText;
 	}
@@ -30,7 +32,7 @@ public class Document implements Iterable<String> {
 	public void setDocumentText(String documentText) {
 		this.documentText = documentText;
 	}
-	
+
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("data/dracula.txt"));
 		int i = 0;
@@ -39,6 +41,8 @@ public class Document implements Iterable<String> {
 			if (i > 100)
 				break;
 		}
+
+		System.out.println("\nType-Token-Relation: " + d.calcTTR());
 	}
 
 	@Override
@@ -46,7 +50,7 @@ public class Document implements Iterable<String> {
 		return new Iterator<String>() {
 
 			StringTokenizer tokenizer = new StringTokenizer(documentText);
-			
+
 			@Override
 			public boolean hasNext() {
 				return tokenizer.hasMoreTokens();
@@ -56,9 +60,20 @@ public class Document implements Iterable<String> {
 			public String next() {
 				return tokenizer.nextToken();
 			}
-			
+
 		};
 	}
-	
-	
+
+	public double calcTTR() {
+		Set<String> types = new HashSet<>();
+		int tokens = 0;
+
+		for (String type : this) {
+			types.add(type);
+			tokens++;
+		}
+
+		return (double) types.size() / tokens;
+	}
+
 }
