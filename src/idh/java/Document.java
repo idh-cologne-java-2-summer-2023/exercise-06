520c6f57f0d332f274745f921c082723e71b22ad
Kbleser <97674016+Inglorious-Inge@users.noreply.github.com>
2023-05-23 23:01:46 +0200

    solved

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -3,7 +3,9 @@ package idh.java;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Document implements Iterable<String> {
@@ -13,16 +15,29 @@ public class Document implements Iterable<String> {
 		FileReader fileReader = new FileReader(f);
 		int ch;
 		StringBuilder b = new StringBuilder();
-		while( (ch = fileReader.read()) != -1 ) {
+		while ((ch = fileReader.read()) != -1) {
 			b.append((char) ch);
 		}
 		fileReader.close();
 		Document doc = new Document();
 		doc.documentText = b.toString();
-		
+
 		return doc;
 	}
-	
+
+	public double ttr() {
+		Set<String> tokenSet = new HashSet<String>();
+		double i = 0;
+		for (String token : this) {
+			i++;
+			tokenSet.add(token);
+		}
+		double d = (double) tokenSet.size();
+		double res = d / i;
+		return res;
+
+	}
+
 	public String getDocumentText() {
 		return documentText;
 	}
@@ -30,15 +45,10 @@ public class Document implements Iterable<String> {
 	public void setDocumentText(String documentText) {
 		this.documentText = documentText;
 	}
-	
+
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("data/dracula.txt"));
-		int i = 0;
-		for (String token : d) {
-			System.out.println(i++ + ": " + token + " ");
-			if (i > 100)
-				break;
-		}
+		System.out.println(d.ttr());
 	}
 
 	@Override
@@ -46,7 +56,7 @@ public class Document implements Iterable<String> {
 		return new Iterator<String>() {
 
 			StringTokenizer tokenizer = new StringTokenizer(documentText);
-			
+
 			@Override
 			public boolean hasNext() {
 				return tokenizer.hasMoreTokens();
@@ -56,9 +66,6 @@ public class Document implements Iterable<String> {
 			public String next() {
 				return tokenizer.nextToken();
 			}
-			
 		};
 	}
-	
-	
 }
--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -3,17 +3,121 @@ package idh.java;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.Stack;
 
 public class Hanoi {
 
+	Stack<Integer> left = new Stack<Integer>();
+	Stack<Integer> middle = new Stack<Integer>();
+	Stack<Integer> right = new Stack<Integer>();
+
 	public Hanoi() {
 		// TODO: Implement
+		for (int i = 9; i > 0; i--) {
+			left.push(i);
+		}
 	}
-	
+
 	private void movePiece(char from, char to) {
 		// TODO: Implement
+		if (checkSize(from, to)) {
+			int temp;
+			if (from == 'l' && to == 'm') {
+				temp = left.pop();
+				middle.push(temp);
+			}
+			if (from == 'l' && to == 'r') {
+				temp = left.pop();
+				right.push(temp);
+			}
+			if (from == 'm' && to == 'l') {
+				temp = middle.pop();
+				left.push(temp);
+			}
+			if (from == 'm' && to == 'r') {
+				temp = middle.pop();
+				right.push(temp);
+			}
+			if (from == 'r' && to == 'l') {
+				temp = right.pop();
+				left.push(temp);
+			}
+			if (from == 'r' && to == 'm') {
+				temp = right.pop();
+				middle.push(temp);
+			} else {
+				System.out.println("Invalid positions.");
+			}
+		} else {
+			System.out.println(from + " is bigger than " + to + ". That is not allowed!");
+		}
 	}
-	
+
+	public boolean checkSize(char from, char to) {
+		if (checkEmpty(to)) {
+			return true;
+		} else {
+			if (from == 'l' && to == 'm') {
+				if (left.peek() < middle.peek()) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+			if (from == 'l' && to == 'r') {
+				if (left.peek() < right.peek()) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+			if (from == 'm' && to == 'l') {
+				if (middle.peek() < left.peek()) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+			if (from == 'm' && to == 'r') {
+				if (middle.peek() < right.peek()) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+			if (from == 'r' && to == 'l') {
+				if (right.peek() < left.peek()) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+			if (from == 'r' && to == 'm') {
+				if (right.peek() < middle.peek()) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean checkEmpty(char to) {
+		switch (to) {
+		case 'l': {
+			return left.empty();
+		}
+		case 'm': {
+			return middle.empty();
+		}
+		case 'r': {
+			return right.empty();
+		}
+		}
+		return false;
+	}
+
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
@@ -29,53 +133,55 @@ public class Hanoi {
 			} catch (Exception e) {
 				System.out.println("Try again, something's not right.");
 				// e.printStackTrace();
-			} 
+			}
 		}
 	}
-	
+
 	private Iterator<Integer> getLeftDescendingIterator() {
 		// TODO: Implement
-		return null;
-
+		return left.iterator();
 	}
+
 	private Iterator<Integer> getMiddleDescendingIterator() {
 		// TODO: Implement
-		return null;
+		return middle.iterator();
 
 	}
+
 	private Iterator<Integer> getRightDescendingIterator() {
 		// TODO: Implement
-		return null;
+		return right.iterator();
 	}
-	
+
 	public String toString() {
 		StringBuilder b = new StringBuilder();
 		b.append("  |\n l|");
 		Iterator<Integer> iter;
 		iter = this.getLeftDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |\n m|");
 		iter = this.getMiddleDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |\n r|");
 		iter = this.getRightDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |");
 		return b.toString();
 	}
-	
+
 	public static void main(String[] args) {
 		Hanoi hanoi = new Hanoi();
 		hanoi.run();
+
 	}
 
 }
