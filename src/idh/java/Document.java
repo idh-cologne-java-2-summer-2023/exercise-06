e3a64f0b41f53af4aadb72d84a838053e52f6337
JohannonBlake <johannon.blade@Gmail.com>
2023-05-23 11:33:46 +0200

    why can´t I call my method in main?!

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -5,9 +5,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 public class Document implements Iterable<String> {
-	String documentText;
+	static String documentText;
 
 	public static Document readFromFile(File f) throws IOException {
 		FileReader fileReader = new FileReader(f);
@@ -31,34 +32,66 @@ public class Document implements Iterable<String> {
 		this.documentText = documentText;
 	}
 	
-	public static final void main(String[] args) throws IOException {
-		Document d = Document.readFromFile(new File("data/dracula.txt"));
-		int i = 0;
-		for (String token : d) {
-			System.out.println(i++ + ": " + token + " ");
-			if (i > 100)
-				break;
-		}
-	}
+	
 //Ah, I see he went witth the "more elegant" option
-	@Override
+	
 	public Iterator<String> iterator() {
 		return new Iterator<String>() {
+			 static ArrayList<String> Tokens = new ArrayList<String>();
+			static ArrayList<String> Types = new ArrayList<String>();
+			//We need the ArrayLists mostly for the type token collection, but I´m putting them here for accessibility
+			//Sidebar: why don´t we do global variables anymore?
 
 			StringTokenizer tokenizer = new StringTokenizer(documentText);
 			
-			@Override
+			
+			
+			
 			public boolean hasNext() {
 				return tokenizer.hasMoreTokens();
 			}
 
-			@Override
-			public String next() {
-				return tokenizer.nextToken();
-			}
 			
+			public String next() {
+				
+				Tokens.add (tokenizer.nextToken());
+				if (Types.contains(Tokens.get(Tokens.size()-1)) == false){
+					Types.add(tokenizer.nextToken());
+				}//Now we have a list of types and a list of tokens
+				TypeTokenRatio(0);
+				return tokenizer.nextToken();
+
+				
+				
+
+			    
+			
+			}
+			public static  double TypeTokenRatio (double ttr){
+				 ttr = Types.size() / Tokens.size();
+				System.out.println("Type token Ratio:" + ttr);
+				return ttr;
+			}
+		
 		};
 	}
 	
+	public  final static void main(String[] args) throws IOException {
+		Document d = Document.readFromFile(new File("data/dracula.txt"));
+		int i = 0;
+		//for (String token : d) {
+		//	System.out.println(i++ + ": " + token + " ");
+		//	if (i > 100)
+		//		break;
+		
 	
-}
+		}
+		
+		
+	
+    
+	}
+
+//}
+	
+
