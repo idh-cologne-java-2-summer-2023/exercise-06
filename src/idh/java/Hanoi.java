7e164ded7150329f35631eb2259617199564d378
ltadsen <larissa45@live.de>
2023-07-13 12:39:30 +0200

    Aufgabe 1 und 2 bearbeitet

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -3,17 +3,53 @@ package idh.java;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Hanoi {
 
+	private Queue<Integer> leftQueue;
+	private Stack<Integer> leftStack;
+	private Stack<Integer> middleStack;
+	private Stack<Integer> rightStack;
+
 	public Hanoi() {
-		// TODO: Implement
+		leftQueue = new LinkedList<>();
+		leftStack = new Stack<>();
+		middleStack = new Stack<>();
+		rightStack = new Stack<>();
+
+		// Scheiben der Größen 9 bis 1 auf dem linken Stab hinzufügen
+		for (int i = 9; i >= 1; i--) {
+			leftQueue.add(i);
+			leftStack.push(i);
+		}
 	}
-	
+
 	private void movePiece(char from, char to) {
-		// TODO: Implement
+		Stack<Integer> sourceStack = getStackByChar(from);
+		Stack<Integer> targetStack = getStackByChar(to);
+
+		if (!sourceStack.isEmpty() && (targetStack.isEmpty() || sourceStack.peek() < targetStack.peek())) {
+			int piece = sourceStack.pop();
+			targetStack.push(piece);
+		}
 	}
-	
+
+	private Stack<Integer> getStackByChar(char c) {
+		switch (c) {
+			case 'l':
+				return leftStack;
+			case 'm':
+				return middleStack;
+			case 'r':
+				return rightStack;
+			default:
+				throw new IllegalArgumentException("Invalid stack character: " + c);
+		}
+	}
+
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
@@ -29,53 +65,55 @@ public class Hanoi {
 			} catch (Exception e) {
 				System.out.println("Try again, something's not right.");
 				// e.printStackTrace();
-			} 
+			}
 		}
 	}
-	
+
+	private Iterator<Integer> getDescendingIterator(Stack<Integer> stack) {
+		Stack<Integer> tempStack = new Stack<>();
+		tempStack.addAll(stack);
+		return tempStack.iterator();
+	}
+
 	private Iterator<Integer> getLeftDescendingIterator() {
-		// TODO: Implement
-		return null;
-
+		return getDescendingIterator(leftStack);
 	}
+
 	private Iterator<Integer> getMiddleDescendingIterator() {
-		// TODO: Implement
-		return null;
+		return getDescendingIterator(middleStack);
+	}
 
-	}
 	private Iterator<Integer> getRightDescendingIterator() {
-		// TODO: Implement
-		return null;
+		return getDescendingIterator(rightStack);
 	}
-	
+
 	public String toString() {
 		StringBuilder b = new StringBuilder();
 		b.append("  |\n l|");
 		Iterator<Integer> iter;
 		iter = this.getLeftDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |\n m|");
 		iter = this.getMiddleDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |\n r|");
 		iter = this.getRightDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |");
 		return b.toString();
 	}
-	
+
 	public static void main(String[] args) {
 		Hanoi hanoi = new Hanoi();
 		hanoi.run();
 	}
-
 }
